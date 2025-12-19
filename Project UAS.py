56fc954c7f5f26a29f5bb50f248b5e14b2121e57
hendriyansah181 <hendriyansah181@gmail.com>
2025-12-19 15:24:53 +0700

    mang lay

--- Project UAS.py
+++ Project UAS.py
@@ -5,3 +5,15 @@ w = int(input("Nilai 2 : "))
 jum = p * w
 print(jum)
 
+data = [
+    {"unit": 1, "harga": 500}
+]
+
+total = 0
+for item in data:
+    total += item["unit"] * item["harga"]
+
+ppn = total * 0.10
+print("Total :", total)
+print("PPN   :", ppn)
+print("Bayar :", total + ppn)
